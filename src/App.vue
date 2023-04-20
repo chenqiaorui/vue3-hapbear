9d017e22a633e6234ae581c4f10a907b199eb562
chenqiaorui <chenqiaorui@staff.easub.com>
2023-04-20 19:07:47 +0800

    added login page by using element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <router-view></router-view>
     </div>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,61 @@ import router from '@/router'
 
 import './assets/main.css'
 
+import {
+    ElButton,
+    ElContainer,
+    ElAside,
+    ElMenu,
+    ElSubMenu,
+    ElMenuItemGroup,
+    ElMenuItem,
+    ElForm,
+    ElFormItem,
+    ElInput,
+    ElCheckbox,
+    ElPopover,
+    ElTag,
+    ElCard,
+    ElTable,
+    ElTableColumn,
+    ElPopconfirm,
+    ElUpload,
+    ElDialog,
+    ElPagination,
+    ElCascader,
+    ElRadioGroup,
+    ElRadio,
+    ElSelect,
+    ElOption
+  } from 'element-plus'
+
 const app = createApp(App) // 生成 Vue 实例 app
 app.use(router) // 引用路由实例
+
+app.use(ElButton)
+  .use(ElContainer)
+  .use(ElAside)
+  .use(ElMenu)
+  .use(ElSubMenu)
+  .use(ElMenuItemGroup)
+  .use(ElMenuItem)
+  .use(ElForm)
+  .use(ElFormItem)
+  .use(ElCheckbox)
+  .use(ElInput)
+  .use(ElPopover)
+  .use(ElTag)
+  .use(ElCard)
+  .use(ElTable)
+  .use(ElTableColumn)
+  .use(ElPopconfirm)
+  .use(ElUpload)
+  .use(ElDialog)
+  .use(ElPagination)
+  .use(ElCascader)
+  .use(ElRadioGroup)
+  .use(ElRadio)
+  .use(ElSelect)
+  .use(ElOption)
 
 app.mount('#app') // 挂载到 #app
